6a1dace7f56186a947882fcd7945acf75b66937d
Tal Kohavy <talkohavy@gmail.com>
2024-09-03 01:15:18 +0300

    wrote the cli tool

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { runCliTool } from './runCliTool.js';
+
+runCliTool();
---
+++ src/runCliTool.ts
@@ -0,0 +1,83 @@
+import { execSync } from 'child_process';
+import os from 'os';
+import { Separator, checkbox, input } from '@inquirer/prompts';
+import { COLORS } from './constants/colors.js';
+import { trimNewLinesAndSpaces } from './utils/trimNewLinesAndSpaces.js';
+
+async function runCliTool() {
+  try {
+    printCliName();
+
+    const prTitle = await getPrTitle();
+    const targetBranch = await getTargetBranch();
+    const reviewers = await selectReviewers();
+
+    const commandToExecute = `az repos pr create --title ${prTitle} --target-branch ${targetBranch} --open ${reviewers ? `--reviewers ${reviewers.join(' ')}` : ''} --squash --auto-complete`;
+
+    console.log(`\n  ${COLORS.green}• Executing: ${COLORS.yellow}${commandToExecute}${COLORS.stop}\n`);
+    execSync(commandToExecute);
+  } catch (_error: any) {
+    console.log(`${os.EOL}${COLORS.red}Bye.${COLORS.stop}${os.EOL}`);
+  }
+}
+
+function printCliName() {
+  console.log(`${COLORS.blue}
+     _     _____    ____                   _          ____  ____  
+    / \\   |__  /   / ___|_ __ ___  __ _  _| |_ ___   |  _ \\|  _ \\ 
+   / _ \\    / /   | |   | '__/ _ \\/ _\` ||_   _/ _ \\  | |_) | |_) |
+  / ___ \\  / /_   | |___| | |  __/ (_| |  | ||  __/  |  __/|  _ < 
+ /_/   \\_\\/____|   \\____|_|  \\___|\\__,_|  |_| \\___|  |_|   |_| \\_\\
+${COLORS.stop}`);
+}
+
+async function getPrTitle() {
+  const currentBranchName = execSync('git branch --show-current').toString();
+  const branchNameWithoutNewLine = trimNewLinesAndSpaces(currentBranchName);
+
+  const prTitle = await input({
+    message: `${COLORS.cyan}Step 1:${COLORS.stop} give PR a title`,
+    default: branchNameWithoutNewLine,
+    theme: { prefix: '✨' },
+  });
+
+  return prTitle;
+}
+
+async function getTargetBranch() {
+  const targetBranch = await input({
+    message: `${COLORS.cyan}Step 2:${COLORS.stop} name of target Branch`,
+    default: 'develop',
+    theme: { prefix: '✨' },
+  });
+
+  return targetBranch;
+}
+
+async function selectReviewers() {
+  const reviewers = await checkbox({
+    message: `${COLORS.cyan}Step 3:${COLORS.stop} Select your PR reviewers`,
+    theme: { prefix: '✨' },
+    choices: [
+      new Separator(),
+      {
+        /* cspell: disable-next-line */
+        name: 'Evgenii',
+        value: 'evgeni@controlup.com',
+        checked: true,
+      },
+      {
+        /* cspell: disable-next-line */
+        name: 'Alex Zaidman',
+        /* cspell: disable-next-line */
+        value: 'alex.zaidman@controlup.com',
+        checked: true,
+      },
+      new Separator(),
+    ],
+  });
+
+  return reviewers;
+}
+
+export { runCliTool };
